ed46992c05610ae0de0ce2ef43688765d4c1cd62
David Nielsen <dn@skm.dk>
2018-01-24 10:39:50 +0100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambda-project/lein-template "0.1.0-SNAPSHOT"
+(defproject lambda-project/lein-template "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
